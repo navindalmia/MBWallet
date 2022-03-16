e3d0debf97e330f8c5cd1686fa113feba24f28ad
navindalmia <navindalmia2003@yahoo.co.in>
2022-03-16 19:55:00 +0000

    Initiating Ratpack API

--- MBWallet/src/main/java/com/api/mb/MBWallet/App.java
+++ MBWallet/src/main/java/com/api/mb/MBWallet/App.java
@@ -1,13 +1,17 @@
 package com.api.mb.MBWallet;
 
+import ratpack.server.RatpackServer;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
         System.out.println( "Hello World!" );
+      RatpackServer.start(server -> server.handlers(chain -> chain
+      .get(ctx -> ctx.render("Welcome to API via ratpack!!!"))));
     }
 }
